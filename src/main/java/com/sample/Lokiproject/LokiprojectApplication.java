9f300ed4f5a9515f83b4ed4957d04b5e293b4b7b
leeebaljith <balajisaravanan97@gmail.com>
2023-07-14 19:09:48 +0530

    spring-lokiPOC initial commit'

---
+++ src/main/java/com/sample/Lokiproject/LokiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sample.Lokiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokiprojectApplication.class, args);
+	}
+
+}
