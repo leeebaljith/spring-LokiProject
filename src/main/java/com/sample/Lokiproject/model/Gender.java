3fe656776b81fc2ce05411d1bffe45a97fcacd8b
leeebaljith <balajisaravanan97@gmail.com>
2023-07-14 20:36:47 +0530

    all necessary classes

---
+++ src/main/java/com/sample/Lokiproject/model/Gender.java
@@ -0,0 +1,7 @@
+package com.sample.Lokiproject.model;
+
+public enum Gender {
+
+    MALE, FEMALE;
+
+}
---
+++ src/main/java/com/sample/Lokiproject/service/PersonCounterService.java
@@ -0,0 +1,29 @@
+package com.sample.Lokiproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
+@Service
+public class PersonCounterService {
+
+    private final Counter counterAdded;
+    private final Counter counterDeleted;
+
+    @Autowired
+    public PersonCounterService(MeterRegistry registry) {
+        this.counterAdded = registry.counter("services.person.add");
+        this.counterDeleted = registry.counter("services.person.deleted");
+    }
+
+    public void countNewPersons() {
+        this.counterAdded.increment();
+    }
+
+    public void countDeletedPersons() {
+        this.counterDeleted.increment();
+    }
+
+}
